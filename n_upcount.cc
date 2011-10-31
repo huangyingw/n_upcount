63ced401435ab2be27e2635546b6c16b3975e3f5
huangyingw@linux_programming <huangyingw@gmail.com>
2011-10-31 22:31:32 +0800

    n

--- n_upcount.cc
+++ n_upcount.cc
@@ -4,25 +4,19 @@
 
 using namespace std;
 
-int nextPerfectSquare(int n)
+int nUpCount(int a[ ], int len, int n)
 {
-  int result=n+1;
-  while (1)
-  {
-    for (int i=0;i<=result;i++)
-    {
-      if (result==i*i)
-        return result;
-    }
-    result++;
-  }
   return 0;
 }
 int main() 
 {
-  cout<<nextPerfectSquare(6)<<endl;//9
-  cout<<nextPerfectSquare(36)<<endl;//49
-  cout<<nextPerfectSquare(0)<<endl;//1
-  cout<<nextPerfectSquare(-5)<<endl;//0
+  int a[] ={2, 3, 1, 0, -6, 8, -3, -1, 2} ;//3
+  cout<<nUpCount(a,sizeof(a)/sizeof(int),5)<<endl;
+
+  int b[]={6, 3, 1};//1
+  cout<<nUpCount(b,sizeof(b)/sizeof(int),5)<<endl;
+
+  int c[]={1, 2, -1, 5, 3, 2, -3};//0
+  cout<<nUpCount(c,sizeof(c)/sizeof(int),20)<<endl;
   return 0;
 }
